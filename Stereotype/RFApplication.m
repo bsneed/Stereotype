7753f587189f084996c5a8f4008cc8b0067c725f
Brandon Sneed <brandon@redf.net>
2013-05-27 14:28:11 -0700

    some cleanup.

--- Stereotype/RFApplication.m
+++ Stereotype/RFApplication.m
@@ -26,18 +26,6 @@
 - (void)sendEvent:(NSEvent *)theEvent
 {
     RFAppDelegate *appDelegate = (RFAppDelegate *)self.delegate;
-    //NSResponder *responder = appDelegate.window.firstResponder;
-    //NSLog(@"responder = %@", responder);
-    //NSLog(@"theEvent = %@", theEvent);
-    
-    /*if (theEvent.type == NSKeyUp && theEvent.keyCode == 49) // 49 = space
-    {
-        if (![responder isKindOfClass:[NSTextView class]])
-            [appDelegate playPauseTrackAction:nil];
-        else
-            NSLog(@"skipping playPause since firstResponder is an NSTextView");
-    }
-    else*/
     if (theEvent.type == NSSystemDefined && theEvent.subtype == SPSystemDefinedEventMediaKeys)
     {
         [appDelegate mediaKeyTap:nil receivedMediaKeyEvent:theEvent];
